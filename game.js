08901f8f30a8d0796634197b79fe26224bbcd5b8
Chaima GHarbi <109389896+Chaimagharbi@users.noreply.github.com>
2022-11-26 13:39:02 +0100

    Game Done! It's working...

--- game.js
+++ game.js
@@ -21,22 +21,48 @@ function playRound(playerSelection, computerSelection) {
     pl = playerSelection.toUpperCase();
     co = computerSelection.toUpperCase();
     if (pl === 'ROCK') {
-        if (co === 'ROCK') return 'It\'s a tie!';
-        else if (co === 'PAPER') return 'You lose! Paper beats Rock!';
-        else if (co === 'SCISSORS') return 'You win! Rock beats Scissors!';
+        if (co === 'ROCK') return 0;
+        else if (co === 'PAPER') return -1;
+        else if (co === 'SCISSORS') return 1;
     }
     else if (pl === 'PAPER') {
-        if (co === 'ROCK') return 'You win! Paper beats Rock!';
-        else if (co === 'PAPER') return 'It\'s a tie!';
-        else if (co === 'SCISSORS') return 'You lose! Scissors beats Paper!';
-    }
-    else if (pl === 'SCISSORS') {
-        if (co === 'ROCK') return 'You lose! Rock beats Scissors!';
-        else if (co === 'PAPER') return 'You win! Scissors beats Paper!';
-        else if (co === 'SCISSORS') return 'It\'s a tie!';
+        if (co === 'ROCK') return 1;
+        else if (co === 'PAPER') return 0;
+        else if (co === 'SCISSORS') return -1;
+    } else if (pl === 'SCISSORS') {
+        if (co === 'ROCK') return -1;
+        else if (co === 'PAPER') return 1;
+        else if (co === 'SCISSORS') return 0;
+    } else {
+        alert('Choose between Rock, Paper and Scissors: ');
+        playRound();
     }
 }
 
-const playerSelection = 'scissors';
+/*const playerSelection = 'scissors';
 const computerSelection = getComputerChoice();
-alert(playRound(playerSelection, computerSelection));
+alert(playRound(playerSelection, computerSelection));*/
+
+function game() {
+    let CoScore = 0, PlScore = 0;
+    for (let i = 0; i < 5; i++) {
+        playerSelection = prompt('Choose between Rock, Paper and Scissors: ');
+        const computerSelection = getComputerChoice();
+        let score = playRound(playerSelection, computerSelection);
+        switch (score) {
+            case 1:
+                PlScore += 1;
+                break;
+            case -1:
+                CoScore += 1;
+                break;
+            default:
+                break;
+        }
+        alert(`Computer ${CoScore}:${PlScore} Player`);
+    }
+    if (CoScore > PlScore) { alert('Winner is ..... COMPUTER!!!');}
+    else if (CoScore < PlScore) { alert('Winner is ..... PLAYER!!!');}
+    else { alert('IT\'S A TIE!!!');}
+}
+game();
