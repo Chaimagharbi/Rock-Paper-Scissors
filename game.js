c9c90134c327d41a1163f50c8960777049a5ef23
Chaima GHarbi <109389896+Chaimagharbi@users.noreply.github.com>
2022-11-26 13:22:01 +0100

    Wrote a prototype function of only one round of RockPaperScissors

--- game.js
+++ game.js
@@ -17,4 +17,26 @@ function getComputerChoice() {
     return choice;
 }
 
-alert(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    pl = playerSelection.toUpperCase();
+    co = computerSelection.toUpperCase();
+    if (pl === 'ROCK') {
+        if (co === 'ROCK') return 'It\'s a tie!';
+        else if (co === 'PAPER') return 'You lose! Paper beats Rock!';
+        else if (co === 'SCISSORS') return 'You win! Rock beats Scissors!';
+    }
+    else if (pl === 'PAPER') {
+        if (co === 'ROCK') return 'You win! Paper beats Rock!';
+        else if (co === 'PAPER') return 'It\'s a tie!';
+        else if (co === 'SCISSORS') return 'You lose! Scissors beats Paper!';
+    }
+    else if (pl === 'SCISSORS') {
+        if (co === 'ROCK') return 'You lose! Rock beats Scissors!';
+        else if (co === 'PAPER') return 'You win! Scissors beats Paper!';
+        else if (co === 'SCISSORS') return 'It\'s a tie!';
+    }
+}
+
+const playerSelection = 'scissors';
+const computerSelection = getComputerChoice();
+alert(playRound(playerSelection, computerSelection));
